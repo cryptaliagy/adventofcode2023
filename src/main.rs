c0b286c285ef0fb148e37aae57ea8c4ced41ffb8
Natalia Maximo <nmaximo@microsoft.com>
2023-12-01 13:39:08 -0500

    advent of code day 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use adventofcode2023::*;
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// The puzzle input ID. Should match with files in the input/ directory
+    id: String,
+
+    /// The part of the puzzle to solve
+    #[arg(short = 'p', default_value = "1")]
+    part: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    let input = match std::fs::read_to_string(format!("inputs/{}.txt", args.id)) {
+        Ok(input) => input,
+        Err(_) => {
+            eprintln!("Could not read input file");
+            std::process::exit(1);
+        }
+    };
+
+    let result = match args.id.as_str() {
+        "1" => match args.part {
+            1 => day_one_part_one(&input),
+            2 => day_one_part_two(&input),
+            _ => {
+                eprintln!("Part {} not found", args.part);
+                std::process::exit(1);
+            }
+        },
+        _ => {
+            eprintln!("Id {} not found", args.id);
+            std::process::exit(1);
+        }
+    };
+
+    println!("{}", result);
 }
