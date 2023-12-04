6b50a7db25fdc8b91b5bdc64ef0ebb3b888f8ae1
Natalia Maximo <nmaximo@microsoft.com>
2023-12-04 01:52:58 -0500

    condensed solution

--- src/day_two.rs
+++ src/day_two.rs
@@ -1,18 +1,16 @@
-use crate::day_two_ast::{Cube, Game, Round};
+use crate::day_two_ast::{Cube, Round};
 use crate::day_two_parser::GamesParser;
 
 pub fn part_one(input: &str) -> u32 {
-    let parser = GamesParser::new();
-
     let maxima = Round::from_cubes(vec![
         Cube(12, "red".to_string()),
         Cube(13, "green".to_string()),
         Cube(14, "blue".to_string()),
     ]);
 
-    let games: Vec<Game> = parser.parse(input).unwrap();
-
-    games
+    GamesParser::new()
+        .parse(input)
+        .unwrap()
         .into_iter()
         .filter(|game| game.is_possible_given(&maxima))
         .map(|game| game.id)
@@ -20,9 +18,10 @@ pub fn part_one(input: &str) -> u32 {
 }
 
 pub fn part_two(input: &str) -> u32 {
-    let parser = GamesParser::new();
-
-    let games: Vec<Game> = parser.parse(input).unwrap();
-
-    games.iter().map(|game| game.power()).sum()
+    GamesParser::new()
+        .parse(input)
+        .unwrap()
+        .into_iter()
+        .map(|game| game.power())
+        .sum()
 }
